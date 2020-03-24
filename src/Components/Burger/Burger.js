71bbcd020fae4a26aabe5cd23987ede00ede8e5b
amirheyvaz <amirheyvaz@gmail.com>
2020-03-24 23:46:28 +0430

    990105

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import './Burger.css'
+import PropTypes from 'prop-types';
+
+const Burger = (props) => {
+
+    return props.children;
+}
+
+Burger.propTypes = {
+
+};
+
+export default Burger;
---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+import './BurgerBuilder.css'
+import PropTypes from 'prop-types';
+
+class BurgerBuilder extends Component {
+
+
+}
+
+BurgerBuilder.propTypes = {
+
+};
+
+export default BurgerBuilder;
