78f6ebaaf92128cf49c5f5673daf62a8a273635f
amirheyvaz <amirheyvaz@gmail.com>
2020-03-31 00:10:28 +0430

    990112-0010

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,11 @@
 import React , {Component} from 'react';
-import './BurgerBuilder.css'
 import PropTypes from 'prop-types';
 import Burger from '../../Components/Burger/Burger';
 import BurgerControls from '../../Components/BurgerControls/BurgerControls';
-import { Row, Col , Container, Modal, Button, Navbar , Nav } from 'react-bootstrap';
+import { Row, Col , Container, Modal, Button, Navbar , Nav, Alert } from 'react-bootstrap';
 import BurgerIngeridientsSummary from '../../Components/BurgerIngeridientsSummary/BurgerIngeridientsSummary';
-
+import Axios from 'axios';
+import classes from './BurgerBuilder.module.css'
 
 const INGREDIENTS_PRICE = {
     salad: 0.8,
@@ -16,6 +16,12 @@ const INGREDIENTS_PRICE = {
 
 class BurgerBuilder extends Component {
 
+    constructor(props){
+        super(props);
+
+        Axios.defaults.baseURL = "http://localhost:2874/api/burgerbuilder/";
+        
+    }
     state = {
         Ingredients: {
             salad: 0,
@@ -25,7 +31,11 @@ class BurgerBuilder extends Component {
         },
         TotalPrice : 4,
         purchasing : false,
-        orderable : false
+        orderable : false,
+        sedingRequest: false,
+        alertVariant : 'success',
+        alertMessage : 'slaamam',
+        alertShow : false
     }
 
     setOrderable = (ingredients) =>{
@@ -80,14 +90,64 @@ class BurgerBuilder extends Component {
         this.setState({purchasing : false});
     }
 
+    checkOutClickHandler = () => {
+        this.setState({sedingRequest : true});
+        var obj = {
+            SaladAmount : this.state.Ingredients.salad,
+            CheeseAmount: this.state.Ingredients.cheese,
+            MeatAmount: this.state.Ingredients.meat,
+            BaconAmount : this.state.Ingredients.bacon
+        };
+        Axios.post("Order" , obj)
+        .then(Response => {
+            if(Response.data){
+                this.setState({
+                    alertMessage : 'Oder Sent Successfully',
+                    alertShow: true,
+                    alertVariant: 'success',
+                    sedingRequest : false,
+                    purchasing: false
+                });
+            }
+            else{
+                this.setState({
+                    alertMessage : 'Oder Sent but there was an error in processing it at the server',
+                    alertShow: true,
+                    alertVariant: 'warning',
+                    sedingRequest : false,
+                    purchasing: false
+                });
+            }
+            
+        })
+        .catch(Error => {
+            this.setState({
+                alertMessage : 'Order didn\'t send to server successfully with error ' + Error,
+                alertShow: true,
+                alertVariant: 'danger',
+                sedingRequest : false,
+                purchasing: false
+            });
+        })
+        .then(() => {
+            this.setState({Ingredients: {
+                salad: 0,
+                meat:0,
+                cheese: 0,
+                bacon: 0
+            }});
+        });
+    }
+
     render(){
         const DisabledInfo = {...this.state.Ingredients};
         for(let [key , value] of Object.entries(DisabledInfo)){
             DisabledInfo[key] = (value <= 0);
         }
         return (
+
             <React.Fragment>
-                
+                     
                 <Modal show={this.state.purchasing} onHide={this.modalCloseHandler}>
                     <Modal.Header closeButton>
                         <Modal.Title>
@@ -98,8 +158,11 @@ class BurgerBuilder extends Component {
                         <BurgerIngeridientsSummary Ingredients={this.state.Ingredients} TotalPrice={this.state.TotalPrice} />
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant='success' style={{width: '100px'}}>
-                            Check Out
+                        <Button variant='success' style={{width: '100px'}}
+                            disabled={this.state.sedingRequest}
+                            onClick={!this.state.sedingRequest ? this.checkOutClickHandler : null}
+                        >
+                            {this.state.sedingRequest ? 'Loading...' : 'Check Out'}
                         </Button>
                     </Modal.Footer>
                 </Modal>
@@ -109,7 +172,9 @@ class BurgerBuilder extends Component {
                         Price = {this.state.TotalPrice.toFixed(2)}  OrderClickHandler={this.OrderClickHandler} orderable={!this.state.orderable}
                     />
                 </div>
-
+                <Alert className={classes.Alert} variant={this.state.alertVariant} show={this.state.alertShow} dismissible onClose={() => {this.setState({alertShow: false});}} >
+                    {this.state.alertMessage}
+                </Alert>
             </React.Fragment>
         );
     }
